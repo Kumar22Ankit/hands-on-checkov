1ba62b712a0100a8390e2fa1d117d820863a188c
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-12-20 17:27:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
-resource "null_resource" "terrateam-test-934984" {
-provisioner "local-exec" {
-  command = "echo Terrateam deployment in progress"
- }
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "insecure-bucket-example"
+
+  acl = "public-read" # Publicly accessible bucket (Checkov flag: Public ACL)
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_policy" "insecure_policy" {
+  bucket = aws_s3_bucket.insecure_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "s3:*",
+        Effect    = "Allow",
+        Resource  = "${aws_s3_bucket.insecure_bucket.arn}/*",
+        Principal = "*"
+      }
+    ]
+  })
 }
