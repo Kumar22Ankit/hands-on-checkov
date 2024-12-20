e53efee90520ed4fe1f114c2fa57b365e216cbb6
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-20 17:09:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "vulnerable_bucket" {
+  bucket = "unique-s3-bucket-${random_id.bucket_id.hex}"
+
+  acl    = "public-read" 
+
+  versioning {
+    enabled = false # Vulnerability: Versioning is not enabled
+  }
+
+  lifecycle_rule {
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+    expiration {
+      days = 365
+    }
+  }
+
+  logging {
+    target_bucket = "" # Vulnerability: No logging enabled
+  }
+
+  tags = {
+    Name        = "Vulnerable S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.vulnerable_bucket.bucket
+}
