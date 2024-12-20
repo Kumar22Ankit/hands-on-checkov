52f6041aed0f49ad98db524e6b07c8858742e0fe
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-12-20 17:17:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,5 @@
-resource "aws_s3_bucket" "vulnerable_bucket1" {
-  bucket = "unique-s3-bucket-${random_id.bucket_id.hex}"
-
-  acl    = "public-read" 
-
-  versioning {
-    enabled = false # Vulnerability: Versioning is not enabled
-  }
-
-  lifecycle_rule {
-    enabled = true
-    transition {
-      days          = 30
-      storage_class = "GLACIER"
-    }
-    expiration {
-      days = 365
-    }
-  }
-
-  logging {
-    target_bucket = "" # Vulnerability: No logging enabled
-  }
-
-  tags = {
-    Name        = "Vulnerable S3 Bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 8
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.vulnerable_bucket.bucket
+resource "null_resource" "terrateam-test" {
+provisioner "local-exec" {
+  command = "echo Terrateam deployment in progress"
+ }
 }
